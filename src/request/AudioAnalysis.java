14846ef4cf9241fb271d672c90006eed3d755084
Jeffrey Ni <jefftiger99@gmail.com>
2014-11-10 09:40:50 -0800

    Inital commit. Migrated audio code from LyricsAnalysis

---
+++ src/request/AudioAnalysis.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package request;
+
+import audioanalysis.*;
+
+/**
+ *
+ * @author Jeffrey
+ */
+public class AudioAnalysis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        SubSong.createSubSong("F:\\Jeffrey\\Desktop\\Science Project 2014-2015\\similarity tests\\art of war\\the art of war.wav");
+    }
+}
